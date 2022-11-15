29f34581d2f6611586576b4d73378052579936fc
ChristianMA19 <68516237+ChristianMA19@users.noreply.github.com>
2022-11-14 19:17:25 -0500

    Update CreacionDB.sql

--- CreacionDB.sql
+++ CreacionDB.sql
@@ -1,38 +1,82 @@
 CREATE TABLE IF NOT EXISTS users(
-    USERNAME VARCHAR(255) NOT NULL,
-    NAME VARCHAR(255) NOT NULL,
-    LAST_NAME VARCHAR(255) NOT NULL,
-    EMAIL VARCHAR(255) NOT NULL,
-    PHONE NUMERIC NOT NULL,
-    PASSWORD VARCHAR(255) NOT NULL,
-    PRIMARY KEY (USERNAME)
+    userid int NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    dateofbirth date NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (userid)
 );
 
-DROP TABLE reviews;
-DROP TABLE viajes;
-DROP TABLE direcciones;
-
-CREATE TABLE IF NOT EXISTS viajes(
-    IDviajes serial primary key,
-    dir_destino varchar(255) NOT NULL,
-    dir_inicio varchar(255) NOT NULL,
-    usermain varchar(255) NOT NULL,
-    pasajeros varchar(255) NOT NULL,
-    num_pasajeros int NOT NULL,
-    numactual_pasajeros int NOT NULL,
-    hora_destino varchar(255) NOT NULL,
-    activo boolean NOT NULL
+CREATE TABLE IF NOT EXISTS factura(
+    idfactura int NOT NULL,
+    fecha timestamp NOT NULL,
+    valor double NOT NULL,
+    userid int NOT NULL,
+    cardid int NOT NULL,
+    descripcion VARCHAR(50) NOT NULL,
+    PRIMARY KEY (idfactura)
 );
 
-CREATE TABLE IF NOT EXISTS reviews(
-    IDviajes int NOT NULL,
-    USERNAME varchar(255) NOT NULL,
-    puntaje int,
-    PRIMARY KEY (IDviajes, USERNAME)
+CREATE TABLE IF NOT EXISTS car_data(
+    carid int NOT NULL,
+    userid int NOT NULL,
+    licenseplate VARCHAR(50) NOT NULL,
+    carbrand VARCHAR(50) NOT NULL,
+    typeofcar VARCHAR(50) NOT NULL,
+    carmodel VARCHAR(50) NOT NULL,
+    PRIMARY KEY (carid)
 );
 
-CREATE TABLE IF NOT EXISTS direcciones(
-    IDdir serial primary key,
-    USERNAME varchar(255) NOT NULL,
-    dir_destino varchar(255) NOT NULL
+CREATE TABLE IF NOT EXISTS parking_spot(
+    spotid int NOT NULL,
+    carid int NOT NULL,
+    parktime time NOT NULL,
+    parkinghistoryid int NOT NULL,
+    parkingsid int NOT NULL,
+    PRIMARY KEY (spotid)
+);
+
+CREATE TABLE IF NOT EXISTS Payment_Methods(
+    userid int NOT NULL,
+    cardholder VARCHAR(50) NOT NULL,
+    cardnumber int NOT NULL,
+    expirationdate date NOT NULL,
+    cvv int NOT NULL,
+    cardtype VARCHAR(50) NOT NULL,
+    billingaddress VARCHAR(50) NOT NULL,
+    cellphonenumber int NOT NULL,
+    PRIMARY KEY (cardid)
+);
+
+CREATE TABLE IF NOT EXISTS order_history(
+    idfactura int NOT NULL,
+    userid int NOT NULL,
+    fecha date NOT NULL,
+    valor double NOT NULL,
+    parkinghistoryid int NOT NULL,
+    PRIMARY KEY (spotid)
+);
+
+CREATE TABLE IF NOT EXISTS parking_history(
+    parkinghistoryid int NOT NULL,
+    spotid int NOT NULL,
+    userid int NOT NULL,
+    parkingsid int NOT NULL,
+    PRIMARY KEY (parkinghistoryid)
+);
+
+CREATE TABLE IF NOT EXISTS parking_details(
+    parkingsid int NOT NULL,
+    numberofspots int NOT NULL,
+    parkinghours time NOT NULL,
+    PRIMARY KEY (parkingsid)
+);
+
+CREATE TABLE IF NOT EXISTS parking_locations(
+    parkingsid int NOT NULL,
+    address VARCHAR(50) NOT NULL,
+    contactnumber int NOT NULL,
+    businessname VARCHAR(50) NOT NULL,
+    PRIMARY KEY (parkingsid)
 );
